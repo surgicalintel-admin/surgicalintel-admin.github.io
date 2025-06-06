5c62f23e3843c7bc0b586b17c09efa779632b4da
Rajesh Koilpillai <rajesh@palmtreeinfotech.com>
2025-06-06 15:30:49 +0530

    removed blog

--- deploy.sh
+++ deploy.sh
@@ -23,8 +23,8 @@ echo "Building React app..."
 pnpm build
 
 echo "Copying blog output into React app's build/blog..."
-mkdir -p $BUILD_DIR/blog
-cp -r $BLOG_DIR/build/* $BUILD_DIR/blog/
+mkdir -p $BUILD_DIR/
+cp -r $BLOG_DIR/build/* $BUILD_DIR/
 
 echo "Preparing deployment branch: $DEPLOY_BRANCH"
 
