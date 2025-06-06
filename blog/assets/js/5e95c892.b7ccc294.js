9d111716a20778de691e39f3d012bba7e88d570b
Preethy <preethy@palmtreeinfotech.com>
2025-06-06 13:08:30 +0530

    Deploy latest React + Blog

---
+++ blog/assets/js/5e95c892.b7ccc294.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[9647],{9148:(e,s,r)=>{r.r(s),r.d(s,{default:()=>o});r(6672);var u=r(3526),a=r(5541),c=r(8154),l=r(4401),n=r(6733),t=r(3420);function o(e){return(0,t.jsx)(a.e3,{className:(0,u.A)(c.G.wrapper.docsPages),children:(0,t.jsx)(n.A,{children:(0,l.v)(e.route.routes)})})}}}]);
