c298e16686274d6f61c6e1b5f04d1c4e31bfc41d
Preethy <preethy@palmtreeinfotech.com>
2025-06-12 10:48:18 +0530

    Deploy latest React + Blog

---
+++ blog/assets/js/87c63677.0de06dc4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[7070],{884:t=>{t.exports=JSON.parse('{"tag":{"label":"burnout","permalink":"/blog/tags/burnout","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/burnout","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
