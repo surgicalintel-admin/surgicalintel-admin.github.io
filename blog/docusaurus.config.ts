60a9989cfc4368f3775e1843f11f13fb79eaad38
Preethy <preethy@palmtreeinfotech.com>
2025-10-31 16:38:49 +0530

    Updated favicon

--- blog/docusaurus.config.ts
+++ blog/docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/healthcare-favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
